0a72fb4d07be982a027189905ce14363b18f9f6c
Ygor Vieira <ygorus@gmail.com>
2020-08-17 14:25:07 -0300

    Estrutura inicial

---
+++ Desafio-Concrete.Domain/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafio_Concrete.Domain.Entities
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
